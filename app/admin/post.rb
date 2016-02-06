d6a648bc1a48e3d8bbb78f9f73cd9ad07d3b80d7
Dan Solo <dan@solocreative.com.au>
2016-02-06 16:48:52 +1100

    Active admin install and setup page and post controllers etc

---
+++ app/admin/post.rb
@@ -0,0 +1,6 @@
+
+ActiveAdmin.register Post do
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  permit_params :title, :content, :body, :image_url, :publisher_id
+end
---
+++ db/migrate/20160206023258_posts.rb
@@ -0,0 +1,10 @@
+class Posts < ActiveRecord::Migration
+  def change
+      create_table :posts do |t|
+        t.string :title
+        t.text   :body
+        t.string :image_url
+        t.timestamps
+      end
+  end
+end
