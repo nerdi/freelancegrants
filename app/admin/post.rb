8b26ad95875cee2cd87425733cffbd391941f938
Dan Solo <dan@solocreative.com.au>
2016-02-11 07:56:49 +1100

    Poach Active Admin Form Setup From AddRedactor Branch

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -1,6 +1,13 @@
-
 ActiveAdmin.register Post do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  permit_params :title, :content, :body, :image_url, :publisher_id, :image
+  permit_params :title, :content, :body, :publisher_id, :image
+
+  form do |f|
+    inputs 'Details' do
+      input :title, :input_html => {:class => "redactor", id: "redactor_here"}
+      input :body, :input_html => {:class => "redactor", id: "redactor_here"}
+      actions
+    end
+  end
 end
