af0a2aadcc74e366781b9ac6d33e26bca3c36220
Dan Solo <dan@solocreative.com.au>
2016-02-13 09:54:05 +1100

    Fix env variables for production 2

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -2,14 +2,16 @@ require "refile/s3"
 
 aws = {
   if Rails.env.production?
-                access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-                secret_access_key: ENV["AWS_SECRET_ACCESS_KEY_ID"],
-              else
-                access_key_id: API_KEYS["AWS_ACCESS_KEY_ID"],
-                secret_access_key: API_KEYS["AWS_SECRET_ACCESS_KEY_ID"],
-              end
-  region: "ap-southeast-2",
-  bucket: "freelancegrants",
+    access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    secret_access_key: ENV["AWS_SECRET_ACCESS_KEY_ID"],
+    region: "ap-southeast-2",
+    bucket: "freelancegrants"
+  else
+    access_key_id: API_KEYS["AWS_ACCESS_KEY_ID"],
+    secret_access_key: API_KEYS["AWS_SECRET_ACCESS_KEY_ID"],
+    region: "ap-southeast-2",
+    bucket: "freelancegrants"
+  end
 }
 Refile.cache = Refile::S3.new(prefix: "cache", **aws)
 Refile.store = Refile::S3.new(prefix: "store", **aws)
