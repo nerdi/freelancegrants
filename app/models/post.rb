2fed713fa20d5e61de38d47a2b7c9a0ed1a22256
Dan Solo <dan@solocreative.com.au>
2016-02-18 18:36:51 +1100

    first ngram test

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,39 @@
 require 'elasticsearch/model'
 class Post < ActiveRecord::Base
-  #add attachement declaration to moidels for refile image uploading
+  #add attachement declaration to models for refile image uploading
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
+
+  #index settings elasticsearch
+
+  settings index: {
+        number_of_shards: 1,
+        analysis: {
+           tokenizer: {
+              ngram_tokenizer: {
+                 type: "nGram",
+                 min_gram: 4,
+                 max_gram: 4
+              }
+           },
+           analyzer: {
+              ngram_tokenizer_analyzer: {
+                 type: "custom",
+                 tokenizer: "ngram_tokenizer"
+              }
+           }
+        }
+     } do
+       mapping dynamic: true do
+         indexes :title, analyzer: "ngram_tokenizer_analyzer"
+         indexes :body, analyzer: "ngram_tokenizer_analyzer"
+       end
+     end
+
+  #validation
   validates_presence_of :title, :body
+
+  #attachment declarations
   attachment :profile_image
   attachment :image
 
@@ -16,7 +46,6 @@ class Post < ActiveRecord::Base
   end
 
   def search(query)
-    #__elasticsearch__.search({query: query})
     __elasticsearch__.search(
       {
         query: {
