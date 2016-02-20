f08816bb8ef3282d92f01004062a785d2ab829da
Dan Solo <dan@solocreative.com.au>
2016-02-20 14:58:53 +1100

    alter search partial to assist with error in heroku

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,7 @@
 class SearchesController < ApplicationController
   def search
     if params[:q].present?
-      @post = Post.search params[:q], fields: [:title, :body], operator: "or", suggest: true
+      @postsearch = Post.search params[:q], fields: [:title, :body], operator: "or", suggest: true
     end
   end
 end
