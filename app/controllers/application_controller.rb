b8f0eb946c92314a9e215d4cbff547651394f7eb
Dan Solo <dan@solocreative.com.au>
2016-03-10 09:52:05 +1100

    ensure superadmin to access activeadmin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :load_footer_components
+  def authenticate_active_admin_user!
+    authenticate_user!
+    unless current_user.superadmin?
+      flash[:alert] = "Unauthorized Access!"
+      redirect_to root_path
+    end
+  end
 
   private
 
