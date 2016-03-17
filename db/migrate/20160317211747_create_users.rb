325b920abd07ecb3560883ee985fef813b933fc0
Dan Solo <dan@solocreative.com.au>
2016-03-18 08:20:58 +1100

    heroku tweak migrations 2

--- db/migrate/20160317211747_create_users.rb
+++ db/migrate/20160317211747_create_users.rb
@@ -1,18 +1,18 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string, :first_name
-      t.string, :last_name
-      t.string, :email
-      t.string, :encrypted_password
-      t.datetime, :reset_password_sent_at
-      t.datetime, :remember_created_at
-      t.integer, :sign_in_count
-      t.datetime, :current_sign_in_at
-      t.datetime, :last_sign_in_at
-      t.inet, :current_sign_in_ip
-      t.inet, :last_sign_in_ip
-      t.boolean, :superadmin
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :encrypted_password
+      t.datetime :reset_password_sent_at
+      t.datetime :remember_created_at
+      t.integer :sign_in_count
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.inet :current_sign_in_ip
+      t.inet :last_sign_in_ip
+      t.boolean :superadmin
       t.string :poo_bear
     end
   end
