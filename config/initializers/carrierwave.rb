944b090192f40c5c34ce8bc369e6b1a52d2a9870
Dan Solo <dan@solocreative.com.au>
2016-03-18 08:06:58 +1100

    heroku tweak

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,12 +6,12 @@ CarrierWave.configure do |config|
     config.dropbox_access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
     config.dropbox_user_id = ENV["USER_ID"]
     config.dropbox_access_type = "app_folder"
-  else
-    config.dropbox_app_key = API_KEYS["APP_KEY"]
-    config.dropbox_app_secret = API_KEYS["APP_SECRET"]
-    config.dropbox_access_token = API_KEYS["ACCESS_TOKEN"]
-    config.dropbox_access_token_secret = API_KEYS["ACCESS_TOKEN_SECRET"]
-    config.dropbox_user_id = API_KEYS["USER_ID"]
-    config.dropbox_access_type = "app_folder"
+  #else
+    #config.dropbox_app_key = API_KEYS["APP_KEY"]
+    #config.dropbox_app_secret = API_KEYS["APP_SECRET"]
+    #config.dropbox_access_token = API_KEYS["ACCESS_TOKEN"]
+    #config.dropbox_access_token_secret = API_KEYS["ACCESS_TOKEN_SECRET"]
+    #config.dropbox_user_id = API_KEYS["USER_ID"]
+    #config.dropbox_access_type = "app_folder"
   end
 end
