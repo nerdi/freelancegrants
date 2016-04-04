428432d22922eaa9c6f38aa75f5855a2a5234d41
Dan Solo <dan@solocreative.com.au>
2016-04-04 21:29:42 +1000

    ensure sidekiq processes mailer in Heroku

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -7,17 +7,17 @@ Rails.application.configure do
 	    :user_name => ENV["SPARKPOST_USERNAME"],
 	    :password  => ENV["SPARKPOST_API_KEY"],
 	    :authentication => 'login',
-	    :domain => 'aspiringwebdev.com'
+	    :domain => 'freelancegrants.com.au'
 	  }
-	  else
-	  	config.action_mailer.smtp_settings = {
-		    :address   => "smtp.sparkpostmail.com",
-		    :port      => 587,
-		    :enable_starttls_auto => true,
-		    :user_name => API_KEYS["SPARKPOST_USERNAME"],
-		    :password  => API_KEYS["SPARKPOST_API_KEY"],
-		    :authentication => 'login',
-		    :domain => 'aspiringwebdev.com'
-		  }
-	  end
+  else
+  	config.action_mailer.smtp_settings = {
+	    :address   => "smtp.sparkpostmail.com",
+	    :port      => 587,
+	    :enable_starttls_auto => true,
+	    :user_name => API_KEYS["SPARKPOST_USERNAME"],
+	    :password  => API_KEYS["SPARKPOST_API_KEY"],
+	    :authentication => 'login',
+	    :domain => 'freelancegrants.com.au'
+	  }
+  end
 end
