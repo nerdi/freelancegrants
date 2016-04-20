755dcbedf6c60c120e8034fc5d4a8c2dff871f90
Dan Solo <dan@solocreative.com.au>
2016-04-20 22:08:06 +1000

    Add modal signup forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -28,4 +28,21 @@ module ApplicationHelper
     number_to_currency(price, :unit => "AUD$")
   end
 
+  def resource_name
+
+  :user
+
+  end
+
+  def resource
+
+  @resource ||= User.new
+
+  end
+
+  def devise_mapping
+
+  @devise_mapping ||= Devise.mappings[:user]
+
+  end
 end
