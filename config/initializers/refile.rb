a2dbeba77d790515a13856d8d9dd5557aa2da427
Dan Solo <dan@solocreative.com.au>
2016-08-11 09:07:31 +1000

    Add correct bucket name to refile

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -4,7 +4,7 @@ if Rails.env.production?
     access_key_id: ENV["AWS_ACCESS_KEY_ID"],
     secret_access_key: ENV["AWS_SECRET_ACCESS_KEY_ID"],
     region: "ap-southeast-2",
-    bucket: "freelancegrants",
+    bucket: "freelancegrant",
   }
   Refile.cache = Refile::S3.new(prefix: "cache", **aws)
   Refile.store = Refile::S3.new(prefix: "store", **aws)
